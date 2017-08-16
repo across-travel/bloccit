31b50f608bbe21400a26cbf1de1d4d8966c82154
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-08-16 17:27:57 +0100

    Assignment 31 Intro CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+
+  	@posts.each do |post|
+  		if post.id % 5 == 0
+  			post.update_attributes(
+  				title: "SPAM",
+  				body: "SPAM"
+  			)
+  		end
+  	end
   end
 
   def show
