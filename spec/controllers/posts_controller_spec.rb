adbcb47e485aaada374c386364094f7f99baeab8
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-08-22 22:58:47 +0100

    Assignment-33-more-crud

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  let(:my_post) { 
+  let(:my_post) {
     Post.create!(
-      title: RandomData.random_sentence, 
+      title: RandomData.random_sentence,
       body: RandomData.random_paragraph
-    ) 
+    )
   }
 
   describe "GET #index" do
@@ -25,12 +25,12 @@ RSpec.describe PostsController, type: :controller do
       get :show, params: {id: my_post.id}
       expect(response).to have_http_status(:success)
     end
-    
+
     it "renders the #show view" do
       get :show, params: {id: my_post.id}
       expect(response).to render_template :show
     end
- 
+
     it "assigns my_post to @post" do
       get :show, params: {id: my_post.id}
       expect(assigns(:post)).to eq(my_post)
@@ -38,7 +38,7 @@ RSpec.describe PostsController, type: :controller do
    end
 
   describe "GET new" do
-  
+
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
@@ -53,7 +53,7 @@ RSpec.describe PostsController, type: :controller do
       get :new
       expect(assigns(:post)).not_to be_nil
     end
-  
+
   end
 
   describe "POST create" do
@@ -71,26 +71,26 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to redirect_to Post.last
     end
 
-  end  
+  end
 
   describe "GET edit" do
     it "returns http success" do
       get :edit, params: {id: my_post.id}
       expect(response).to have_http_status(:success)
     end
- 
+
     it "renders the #edit view" do
       get :edit, params: {id: my_post.id}
  # #1
       expect(response).to render_template :edit
     end
- 
+
  # #2
     it "assigns post to be updated to @post" do
       get :edit, params: {id: my_post.id}
- 
+
       post_instance = assigns(:post)
- 
+
       expect(post_instance.id).to eq my_post.id
       expect(post_instance.title).to eq my_post.title
       expect(post_instance.body).to eq my_post.body
@@ -101,20 +101,20 @@ RSpec.describe PostsController, type: :controller do
     it "updates post with expected attributes" do
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
- 
+
       put :update, params: {id: my_post.id, post: {title: new_title, body: new_body}}
- 
+
  # #3
       updated_post = assigns(:post)
       expect(updated_post.id).to eq my_post.id
       expect(updated_post.title).to eq new_title
       expect(updated_post.body).to eq new_body
     end
- 
+
     it "redirects to the updated post" do
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
- 
+
  # #4
       put :update, params: {id: my_post.id, post: {title: new_title, body: new_body}}
       expect(response).to redirect_to my_post
@@ -128,7 +128,7 @@ RSpec.describe PostsController, type: :controller do
       count = Post.where({id: my_post.id}).size
       expect(count).to eq 0
     end
- 
+
     it "redirects to posts index" do
       delete :destroy, params: {id: my_post.id}
  # #7
