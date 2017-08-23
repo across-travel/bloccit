3257060380a8900567a816f3b6e314817e6fca78
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-08-23 17:00:29 +0100

    Assignment-33-more-crud Complete

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Question, type: :model do
 
   describe "attributes" do
     it "has title and body attributes" do
-      expect(:question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: true)
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: true)
     end
   end
 end
