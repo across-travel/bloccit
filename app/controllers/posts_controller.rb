93db1dd3a829633904ab2fbafd0caae93a3781e0
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-09-02 17:59:16 +0100

    Checkpoint-34-topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
@@ -9,6 +6,7 @@ class PostsController < ApplicationController
 
   def new
   	@post = Post.new
+    @topic = Topic.find(params[:topic_id])
   end
 
   def create
@@ -16,12 +14,14 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
 
+    @post.topic = @topic
  # #10
     if @post.save
  # #11
       flash[:notice] = "Post was saved successfully."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
  # #12
       flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -37,10 +37,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
- 
+
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :edit
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
  # #8
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :posts
 
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   get 'about', to: 'welcome#about'
 
   root 'welcome#index'
