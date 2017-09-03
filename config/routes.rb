e6a47f692167429725563192773d25f62c87f8db
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-09-03 20:11:54 +0100

    checkpoint-36-sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+
+  resources :users, only: [:new, :create]
+
   get 'about', to: 'welcome#about'
 
   root 'welcome#index'
