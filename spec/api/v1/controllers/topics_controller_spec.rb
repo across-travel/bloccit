03fa9ef154936155a065f621f80e1d238e7aa3ce
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-09-29 15:23:50 +0100

    Checkpoint-49-building_apis

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -14,6 +14,21 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       get :show, params: {id: my_topic.id}
       expect(response).to have_http_status(:success)
     end
+
+    it "PUT update returns http unauthenticated" do
+      put :update, params: {id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}}
+      expect(response).to have_http_status(401)
+    end
+
+    it "POST create returns http unauthenticated" do
+      post :create, params: {topic: {name: "Topic Name", description: "Topic Description"}}
+      expect(response).to have_http_status(401)
+    end
+
+    it "DELETE destroy returns http unauthenticated" do
+      delete :destroy, params: {id: my_topic.id}
+      expect(response).to have_http_status(401)
+    end
   end
 
   context "unauthorized user" do
@@ -29,6 +44,86 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
     it "GET show returns http success" do
       get :show, params: {id: my_topic.id}
       expect(response).to have_http_status(:success)
+    end
+
+    it "PUT update returns http forbidden" do
+      put :update, params: {id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}}
+      expect(response).to have_http_status(403)
+    end
+
+    it "POST create returns http forbidden" do
+      post :create, params: {topic: {name: "Topic Name", description: "Topic Description"}}
+      expect(response).to have_http_status(403)
+    end
+
+    it "DELETE destroy returns http forbidden" do
+      delete :destroy, params: {id: my_topic.id}
+      expect(response).to have_http_status(403)
+    end
+  end
+
+  context "authenticated and authorized users" do
+    before do
+      my_user.admin!
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+      @new_topic = build(:topic)
+    end
+
+    describe "PUT update" do
+      before { put :update, params: {id: my_topic.id, topic: {name: @new_topic.name, description: @new_topic.description}} }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "updates a topic with the correct attributes" do
+        updated_topic = Topic.find(my_topic.id)
+        hashed_json = JSON.parse(response.body)
+        expect(hashed_json['name']).to eq(updated_topic.name)
+        expect(hashed_json['description']).to eq(updated_topic.description)
+      end
+    end
+
+    describe "POST create" do
+      before { post :create, params: {topic: {name: @new_topic.name, description: @new_topic.description} }}
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "creates a topic with the correct attributes" do
+        hashed_json = JSON.parse(response.body)
+        expect(hashed_json["name"]).to eq(@new_topic.name)
+        expect(hashed_json["description"]).to eq(@new_topic.description)
+      end
+    end
+
+    describe "DELETE destroy" do
+      before { delete :destroy, params: {id: my_topic.id} }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "returns the correct json success message" do
+        expect(response.body).to eq({ message: "Topic destroyed", status: 200 }.to_json)
+      end
+
+      it "deletes my_topic" do
+        expect{ Topic.find(my_topic.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
