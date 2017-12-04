f43778d3fcfdd9556e1d20c04baefb11547a2d5c
Ihtishaam Ahmed <ihtishaam_ahmed123@hotmail.com>
2017-12-04 20:38:18 +0000

    Sign in for invite tests

--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe InvitesController, type: :controller do
+  let(:my_user) { create(:user) }
 
   describe "GET #new" do
+    before do
+      create_session(my_user)
+    end
+
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
